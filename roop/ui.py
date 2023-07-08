3de5b17757518bce8836f8d08d7f2a2c8b21f9e5
x0unmaze <137311237+x0unmaze@users.noreply.github.com>
2023-07-08 11:04:52 +0700

    update ui.py

--- roop/ui.py
+++ roop/ui.py
@@ -218,8 +218,8 @@ def init_preview() -> None:
 def update_preview(frame_number: int = 0) -> None:
     if roop.globals.source_path and roop.globals.target_path:
         temp_frame = get_video_frame(roop.globals.target_path, frame_number)
-        if predict_frame(temp_frame):
-            quit()
+        # if predict_frame(temp_frame):
+        #     quit()
         for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
             temp_frame = frame_processor.process_frame(
                 get_one_face(cv2.imread(roop.globals.source_path)),
